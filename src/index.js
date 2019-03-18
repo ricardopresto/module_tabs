b6d9d6dc01fa6eef7721b985be767f5fcd8c581d
ricardopresto <ricardopresto5@gmail.com>
2019-03-18 17:53:16 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import { main_page, second_page, third_page } from "./pages";
+
+main_page();
---
+++ src/pages.js
@@ -0,0 +1,32 @@
+const main_page = function() {
+  const headline = document.createElement("h1");
+  headline.textContent = "Headline";
+  const main_image = document.createElement("img");
+  main_image.src = "../dist/sunflowers.jpg";
+  const body_text = document.createElement("p");
+  body_text.textContent = `Magna cupidatat Lorem velit voluptate
+   mollit aute amet pariatur aliqua velit ipsum. Enim cupidatat 
+   minim non deserunt officia culpa aliquip sunt commodo irure 
+   consectetur nostrud. Commodo magna tempor Lorem id aute sint 
+   eu. Ad aliqua dolor amet aute excepteur. Commodo incididunt 
+   proident laborum eu minim Lorem amet ad velit cillum ut tempor. 
+   Minim velit duis dolor duis mollit dolor Lorem culpa occaecat in.
+   Fugiat aliqua voluptate amet proident. Irure veniam aute id ex 
+   cupidatat magna laboris consectetur mollit ullamco id. Ea 
+   adipisicing minim sint duis sit. Occaecat voluptate irure sint 
+   aute duis ad minim occaecat nulla proident officia dolore magna 
+   reprehenderit. Duis dolore incididunt dolore adipisicing ullamco 
+   ex velit elit.`;
+
+  const content = document.getElementById("content");
+
+  content.appendChild(headline);
+  content.appendChild(main_image);
+  content.appendChild(body_text);
+};
+
+const second_page = function() {};
+
+const third_page = function() {};
+
+export { main_page, second_page, third_page };
